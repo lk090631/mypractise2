284029cf46b74ee0becae717443307671686ca94
lk090631 <129963866+lk090631@users.noreply.github.com>
2024-12-12 13:29:17 +0530

    Update lokesh.cpp

--- lokesh.cpp
+++ lokesh.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     /* code */
+    cout<<"Tum Raja Ho Kahi Ke"<<"\n";
     cout<<"Aaj Hum Sikhe Ge Git GitHub"<<"\n";
     cout<<"Hum Honge KaamiYaab Ek Din"<<"\n";
 }
